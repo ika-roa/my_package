48cca11478c72588860be21cb4c15a178d403865
Stefanie Mak <mak@dynamic-biosensors.com>
2022-10-14 10:16:03 +0200

    feat: Add package setup files

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+"""
+    Minimal setup.py file, actual configuration information is in setup.cfg
+"""
+
+from setuptools import setup
+
+if __name__ == "__main__":
+    setup()
